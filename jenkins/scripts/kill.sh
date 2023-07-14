a30ecff6e9e6f0cf19b1d112d8b1b1429634956c
Ridwan <ridwansusilo36@gmail.com>
2023-07-15 00:03:56 +0700

    add deploy commitss

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -3,5 +3,8 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
+npm start & echo $! > .pidfile
+sleep 10
 set -x
 kill $(cat .pidfile)
+rm .pidfile
